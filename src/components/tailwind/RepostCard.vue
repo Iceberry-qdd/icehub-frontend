fca508e3b38c12caff35761ed8834c7be132530e
Iceberry-qdd <qddwork@outlook.com>
2023-02-19 22:29:30 +0800

    修复RepostCard组件不显示gif标识问题

--- src/components/tailwind/RepostCard.vue
+++ src/components/tailwind/RepostCard.vue
@@ -7,9 +7,13 @@
             <div class="text-gray-400 top-[1px]">发布于 {{ formattedTime }}</div>
         </div>
         <div class="px-2 break-all text-[11pt]">{{ state.post.content }}</div>
-        <div class="flex flex-col gap-y-1">
-            <img v-if="state.post.attachmentsUrl.length > 0" :src="getCoverImageUrl(state.post.attachmentsUrl[0])"
+        <div class="flex relative flex-col gap-y-1">
+            <img loading="lazy" v-if="state.post.attachmentsUrl.length > 0" :src="getCoverImageUrl(state.post.attachmentsUrl[0])"
                 class=" rounded-b-[8px] w-full h-[15rem] object-cover" />
+            <div v-if="isGifCover"
+                class="absolute flex justify-center items-center w-full h-full top-0 right-0  text-white cursor-pointer">
+                <IconGif class="w-[2.5rem] h-[2.5rem] rounded-full bg-[#000000BB] gif"></IconGif>
+            </div>
         </div>
     </div>
 </template>
@@ -24,6 +28,7 @@
 import { computed, reactive } from 'vue'
 import { humanizedTime } from '@/utils/formatUtils.js'
 import router from '@/route.js'
+import IconGif from '@/components/icons/IconGif.vue'
 
 const props = defineProps(['post'])
 
@@ -40,12 +45,17 @@ function routeToUserProfile() {
 }
 
 const avatar = computed(() => {
-    const {previewUrl,originUrl} = state.post.user.avatarUrl || [null,null]
+    const { previewUrl, originUrl } = state.post.user.avatarUrl || [null, null]
     const defaultUrl = `https://api.multiavatar.com/${state.post.user.nickname}.svg`
-    return previewUrl||originUrl||defaultUrl
+    return previewUrl || originUrl || defaultUrl
 })
 
 function getCoverImageUrl(attachment) {
     return attachment.previewUrl || attachment.originUrl
 }
+
+const isGifCover = computed(() => {
+    if (!state.post.attachmentsUrl[0]) return false
+    else return state.post.attachmentsUrl[0].contentType == 'image/gif'
+})
 </script>
