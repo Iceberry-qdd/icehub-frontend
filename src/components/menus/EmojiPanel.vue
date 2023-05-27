62aba121c55dd5b2353c266e27e5b88ccb7641b3
Iceberry-qdd <qddwork@outlook.com>
2023-05-27 09:54:36 +0800

    完成emoji表情面板历史表情功能

--- src/components/menus/EmojiPanel.vue
+++ src/components/menus/EmojiPanel.vue
@@ -1,21 +1,24 @@
 <template>
-    <div class="panel overflow-y-scroll overflow-x-hidden min-h-[8rem] px-1 pr-[1px] max-h-[18rem] bg-white rounded-[6px] shadow-md ring-1 ring-gray-200">
-        <div class="category sticky z-[99] top-0 py-2 px-2 grid gap-1 grid-cols-9 bg-white border-b-[1px]">
-            <button v-for="(emoji, index) in status.category" :key="index" :title="categoryZh[emoji.category]"
-            @click="changeCategory(emoji.category)"
+    <div
+        class="panel overflow-y-scroll overflow-x-hidden min-h-[8rem] px-1 pr-[1px] max-h-[18rem] bg-white rounded-[6px] shadow-md ring-1 ring-gray-200">
+        <div class="category sticky z-[99] top-0 py-2 px-2 grid gap-1 grid-cols-9 bg-white border-b-[1px]"
+            v-if="state.historyEmojis.length > 0">
+            <button v-for="(emoji, index) in state.historyEmojis" :key="index" :title="categoryZh[emoji.category]"
+                @click="chooseEmoji(emoji)"
                 class="flex aria-selected justify-center items-center w-[1.75rem] h-[1.75rem] p-1 border border-transparent rounded-[8px] cursor-pointer hover:bg-[#f1f3f4] focus:bg-[#cfe2ff]">
                 <span :style="{ backgroundPosition: calcSpriteSheet(emoji.sheet_x, emoji.sheet_y) }"
-                            class="w-[20px] h-[20px] bg-[length:6100%_6100%] bg-[url('https://cdn.jsdelivr.net/npm/emoji-datasource-apple@15.0.0/img/apple/sheets-256/64.png')]"></span>
+                    class="w-[20px] h-[20px] bg-[length:6100%_6100%] bg-[url('https://cdn.jsdelivr.net/npm/emoji-datasource-apple@15.0.0/img/apple/sheets-256/64.png')]"></span>
             </button>
         </div>
 
         <div class="">
-            <div v-for="(emojis, category) in status.emojiMap" :key="category">
-                <div class="bg-[#FFFFFFDD] z-[98] py-1 px-2 text-[11pt] backdrop-blur-sm sticky top-[2.8rem]" :id="category">
+            <div v-for="(emojis, category) in state.emojiMap" :key="category">
+                <div :class="[state.historyEmojis.length > 0 ? 'top-[2.8rem]' : 'top-0']"
+                    class="bg-[#FFFFFFDD] z-[98] py-1 px-2 text-[11pt] backdrop-blur-sm sticky" :id="category">
                     {{ categoryZh[category] }}
                 </div>
                 <div class="grid p-2 gap-2 grid-cols-6">
-                    <button @click="chooseEmoji(emoji.short_name)" v-for="(emoji) in emojis" :key="emoji.unified" :title="emoji.name"
+                    <button @click="chooseEmoji(emoji)" v-for="(emoji) in emojis" :key="emoji.unified" :title="emoji.name"
                         class="flex justify-center items-center w-[2.5rem] h-[2.5rem] p-1 border text-[16pt] border-transparent rounded-[8px] cursor-pointer hover:bg-[#f1f3f4] focus:bg-[#cfe2ff]">
                         <span :style="{ backgroundPosition: calcSpriteSheet(emoji.sheet_x, emoji.sheet_y) }"
                             class="w-[24px] h-[24px] bg-[length:6100%_6100%] bg-[url('https://cdn.jsdelivr.net/npm/emoji-datasource-apple@15.0.0/img/apple/sheets-256/64.png')]"></span>
@@ -45,7 +48,7 @@
 </style>
 
 <script setup>
-import { reactive,onMounted,onUnmounted } from 'vue'
+import { reactive } from 'vue'
 import emojiPack from 'https://cdn.jsdelivr.net/npm/emoji-datasource-apple@15.0.0/+esm'
 
 const emit = defineEmits(['emojiName'])
@@ -67,11 +70,12 @@ const emojis = category.reduce((obj, key) => (
     obj[key] = emojiPack.filter(e => e.category === key && e.has_img_apple === true)
         .sort((a, b) => a.sort_order - b.sort_order),
     obj), {})
-const categoryPicker = category.map(c=>emojis[c][0])
+const categoryPicker = category.map(c => emojis[c][0])
 
-const status = reactive({
+const state = reactive({
     category: categoryPicker,
-    emojiMap: emojis
+    emojiMap: emojis,
+    historyEmojis: JSON.parse(localStorage.getItem('historyEmoji')) || []
 })
 
 function convertToEmoji(unified) {
@@ -82,29 +86,24 @@ function calcSpriteSheet(sheet_x, sheet_y) {
     return `${100 / 60 * sheet_x}% ${100 / 60 * sheet_y}%`
 }
 
-function changeCategory(category){ // TODO 此函数没完成
-    console.log(category)
-    const scrollBarTop=getScrollTop()
-
-    // document.getElementById(category).scrollBy({top:100,behavior:'smooth'})
-    document.getElementById(category).scrollIntoView({ behavior: 'smooth',block:'start' })
-    // const scrollBarTop2=getScrollTop()
-    // console.log(`滚动条高度差：${scrollBarTop2-scrollBarTop}`)
-    // window.scrollBy(0,0)
+function chooseEmoji(emoji) {
+    storeEmojiToLocalStorage(emoji)
+    emit('emojiName', [emoji.short_name])
 }
 
-function getScrollTop(){
-    var scrollTop = 0
-    if(document.documentElement && document.documentElement.scrollTop){
-        scrollTop = document.documentElement.scrollTop
-    }else if(document.body){
-        scrollTop = document.body.scrollTop
+function storeEmojiToLocalStorage(emoji) {
+    const KEY = 'historyEmoji'
+    let historyEmojis = state.historyEmojis
+    while (historyEmojis.length >= 9) {
+        historyEmojis.pop()
     }
-    return scrollTop
-}
+    historyEmojis.unshift(emoji)
 
-function chooseEmoji(name){
-    // console.log(name)
-    emit('emojiName',[name])
+    const obj = {}
+    historyEmojis = historyEmojis.reduce((total, next) => {
+        obj[next.name] ? '' : obj[next.name] = true && total.push(next)
+        return total
+    }, [])
+    localStorage.setItem(KEY, JSON.stringify(historyEmojis))
 }
 </script>
