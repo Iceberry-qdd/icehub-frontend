6275863bd380e0247d37c657799b6084e9f264ce
Iceberry <qddwork@outlook.com>
2024-05-04 10:59:15 +0800

    jsdelivr依赖替换为本地打包

--- src/indexApp/extensions/katexExt.js
+++ src/indexApp/extensions/katexExt.js
@@ -1,11 +1,29 @@
-import showdownKatex from "showdown-katex"
+import renderMathInElement from "katex/contrib/auto-render/auto-render"
+import 'katex/dist/katex.min.css'
 
-export const katexExt = (showdownKatex({
-    displayMode: true,
-    throwOnError: false,
-    errorColor: '#ff0000',
+const parse = new DOMParser()
+const params = {
+    // customised options
+    // • auto-render specific keys, e.g.:
     delimiters: [
-        { left: '$', right: '$', display: false },
-        { left: '~', right: '~', display: true, asciimath: true },
-    ]
-}))()[0]
+        {left: '$$', right: '$$', display: true},
+        {left: '$', right: '$', display: false},
+        {left: "\\(", right: "\\)", display: false},
+        {left: "\\begin{equation}", right: "\\end{equation}", display: true},
+        {left: "\\begin{align}", right: "\\end{align}", display: true},
+        {left: "\\begin{alignat}", right: "\\end{alignat}", display: true},
+        {left: "\\begin{gather}", right: "\\end{gather}", display: true},
+        {left: "\\begin{CD}", right: "\\end{CD}", display: true},
+        {left: "\\[", right: "\\]", display: true}
+    ],
+    // • rendering keys, e.g.:
+    throwOnError : false
+}
+export const katexExt = {
+    type: 'output',
+    filter: function(text, converter, options){
+        const node = parse.parseFromString(text, 'text/html').body
+        renderMathInElement(node, params)
+        return node.innerHTML
+    }
+}
